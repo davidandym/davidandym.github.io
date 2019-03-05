a426f04b5dcf54aa81ba804464031ed1d3f115ee
David Mueller <daveandym@gmail.com>
2019-03-05 14:33:17 -0500

    Updates

---
+++ precache-manifest.d103ad751b170dd2d0cabf926b420d7d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "35197eb9aac45831b14f",
+    "url": "/static/css/main.cfb90a19.chunk.css"
+  },
+  {
+    "revision": "35197eb9aac45831b14f",
+    "url": "/static/js/main.35197eb9.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "f467bcf7d4498a2486c9",
+    "url": "/static/js/2.f467bcf7.chunk.js"
+  },
+  {
+    "revision": "62816157a86078ef1959d6ae0effe179",
+    "url": "/static/media/my-face.62816157.jpg"
+  },
+  {
+    "revision": "0d70334be1b2521a951d514d35337767",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.e0c07f9f5ecacce79836ab200aaddd9c.js"
+  "/precache-manifest.d103ad751b170dd2d0cabf926b420d7d.js"
 );
 
 workbox.clientsClaim();
