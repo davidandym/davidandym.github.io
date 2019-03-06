9df6f30bd6cc518f5dcdb43d25936a24b9a5e10c
David Mueller <daveandym@gmail.com>
2019-03-06 14:55:06 -0500

    more things

--- src/components/Pictures.js
+++ src/components/Pictures.js
@@ -1,12 +1,35 @@
 import React, { Component, Text } from 'react';
-import Card from '@material-ui/core/Card';
 import pics from './../assets/pics/pics.json'
+import Lightbox from 'react-images'
 import '../css/pictures.css'
 
 class Pics extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            show_lightbox: true,
+            cur_pic: 0
+        }
+
+        this.close_lightbox = this.close_lightbox.bind(this);
+        this.next_img = this.next_img.bind(this);
+        this.prev_img = this.prev_img.bind(this);
+    }
+
+    close_lightbox() {
+        this.setState({show_lightbox: false});
+    }
+
+    next_img() {
+        this.setState({cur_pic: this.state.cur_pic + 1})
+    }
+
+    prev_img() {
+        this.setState({cur_pic: this.state.cur_pic - 1})
+    }
 
     render() {
-        var pic_items = pics.pics.map((value, _) => {
+        var pic_gallery = pics.pics.map((value, _) => {
             return (
                 <Picture
                     key={value.src}
@@ -15,15 +38,28 @@ class Pics extends Component {
                 />
             )
         });
+
+        var lightbox_pics = pics.pics.map((value, _) => {
+            return {
+                src: require(`../assets/pics/${value.src}`),
+                caption: value.desc
+            }
+        })
         
         return(
             <div className="pics-outer">
                 <div className="pics-intro">
                     Just some moments from my PhD life that seemed worth sharing.
                 </div>
-                <div className="pics-container">
-                    {pic_items}
-                </div>
+                <Lightbox
+                    currentImage={this.state.cur_pic}
+                    images={lightbox_pics}
+                    isOpen={this.state.show_lightbox}
+                    onClickPrev={this.prev_img}
+                    onClickNext={this.next_img}
+                    onClose={this.close_lightbox}
+                />
+                {pic_gallery}
             </div>
         )
     }
@@ -32,10 +68,10 @@ class Pics extends Component {
 class Picture extends Component {
     render() {
         return (
-            <Card className={"pics-card"} raised={true}>
+            <div className={"pics-card"}>
                 <img className="pics-img" src={require("../assets/pics/" + this.props.img_src)}/>
                 <p className="pics-desc">{this.props.desc}</p>
-            </Card>
+            </div>
         )
     }
 }
