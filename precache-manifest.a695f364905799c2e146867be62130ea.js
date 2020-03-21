20e46d864670d1e2b68251910b550d8f1759ce03
David Mueller <dam@Davids-MacBook-Pro.local>
2020-03-21 11:26:45 -0400

    Updates

---
+++ precache-manifest.a695f364905799c2e146867be62130ea.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "2e593da57d8d12f48d7d",
+    "url": "/static/css/main.c965d3ce.chunk.css"
+  },
+  {
+    "revision": "2e593da57d8d12f48d7d",
+    "url": "/static/js/main.2e593da5.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "61a805be5222d6fe2fda",
+    "url": "/static/js/2.61a805be.chunk.js"
+  },
+  {
+    "revision": "d448f5a4c0bbae56f0cdf44110147fab",
+    "url": "/static/media/my-face.d448f5a4.jpg"
+  },
+  {
+    "revision": "1edac36ad6ce1afe5fc1a3f5176c9ef0",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.e4cb44f5fe734dd48a81593cb545e974.js"
+  "/precache-manifest.a695f364905799c2e146867be62130ea.js"
 );
 
 workbox.clientsClaim();
@@ -30,5 +30,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute("/index.html", {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
