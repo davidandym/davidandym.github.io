572fafd23fc1a5fa9370fed4a25262fbc233d81b
David Mueller <daveandym@gmail.com>
2020-12-20 13:40:14 -0500

    Updates

--- precache-manifest.7d0c53e78e20d0f1818021c7865af7d3.js
+++ precache-manifest.7d0c53e78e20d0f1818021c7865af7d3.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = [
   {
-    "revision": "9c107817a2a8bf9860fc",
+    "revision": "3dc71156a40d9a6e1a57",
     "url": "/static/css/main.d5aadd5a.chunk.css"
   },
   {
-    "revision": "9c107817a2a8bf9860fc",
-    "url": "/static/js/main.9c107817.chunk.js"
+    "revision": "3dc71156a40d9a6e1a57",
+    "url": "/static/js/main.3dc71156.chunk.js"
   },
   {
     "revision": "fdfcfda2d9b1bf31db52",
@@ -20,28 +20,28 @@ self.__precacheManifest = [
     "url": "/static/media/my-face.d448f5a4.jpg"
   },
   {
-    "revision": "d4132103d3124487b5209577da7ffb9d",
-    "url": "/static/media/Neurips2020.d4132103.ipynb"
+    "revision": "5c186ae2c7e4e2126f1510763e1aedf1",
+    "url": "/static/media/Neurips2020.5c186ae2.ipynb"
   },
   {
     "revision": "24b3301075a0c18a53073a4d152a1f36",
     "url": "/static/media/crf_sampling.24b33010.ipynb"
   },
   {
-    "revision": "1afcbd6a1c454e7b7f0bc58155558b9d",
-    "url": "/static/media/academicons.1afcbd6a.ttf"
+    "revision": "2552b354cabf01815628a244fde0636e",
+    "url": "/static/media/academicons.2552b354.woff"
   },
   {
     "revision": "ea3755c311d0678ca4f041912a9039d6",
     "url": "/static/media/academicons.ea3755c3.eot"
   },
   {
-    "revision": "406b38d29cffe8ffcb7962faa2e095ef",
-    "url": "/static/media/academicons.406b38d2.svg"
+    "revision": "1afcbd6a1c454e7b7f0bc58155558b9d",
+    "url": "/static/media/academicons.1afcbd6a.ttf"
   },
   {
-    "revision": "2552b354cabf01815628a244fde0636e",
-    "url": "/static/media/academicons.2552b354.woff"
+    "revision": "406b38d29cffe8ffcb7962faa2e095ef",
+    "url": "/static/media/academicons.406b38d2.svg"
   },
   {
     "revision": "674f50d287a8c48dc19ba404d20fe713",
@@ -64,7 +64,7 @@ self.__precacheManifest = [
     "url": "/static/media/fontawesome-webfont.912ec66d.svg"
   },
   {
-    "revision": "e199239b80681439104ef63ee3802526",
+    "revision": "c600afd1dc5995e8f01177228d4a7a3f",
     "url": "/index.html"
   }
 ];
