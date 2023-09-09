dc164b30bec23c8b735ff2112ec68007766450b8
David Mueller <daveandym@gmail.com>
2023-09-09 12:50:29 -0400

    Updates

--- precache-manifest.005d00d1a8c692bc3bcf499ad03e05e2.js
+++ precache-manifest.005d00d1a8c692bc3bcf499ad03e05e2.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = [
   {
-    "revision": "21e1345c4fc6dc231a3a",
-    "url": "/static/css/main.bf5c1670.chunk.css"
+    "revision": "4a13fc18032f434bcf02",
+    "url": "/static/css/main.60c3021a.chunk.css"
   },
   {
-    "revision": "21e1345c4fc6dc231a3a",
-    "url": "/static/js/main.21e1345c.chunk.js"
+    "revision": "4a13fc18032f434bcf02",
+    "url": "/static/js/main.4a13fc18.chunk.js"
   },
   {
     "revision": "fdfcfda2d9b1bf31db52",
     "url": "/static/js/runtime~main.fdfcfda2.js"
   },
   {
-    "revision": "748e8bcc1febb30fab87",
-    "url": "/static/js/2.748e8bcc.chunk.js"
+    "revision": "eca4f34ca259e0c529f8",
+    "url": "/static/js/2.eca4f34c.chunk.js"
   },
   {
     "revision": "484e84bb71b76e4b00120ca60ec80a3c",
@@ -36,6 +36,10 @@ self.__precacheManifest = [
     "url": "/static/media/GradientCovariance.e653f856.ipynb"
   },
   {
+    "revision": "646646dd4a18835e3a7dadb8486c6395",
+    "url": "/static/media/MTL.646646dd.ipynb"
+  },
+  {
     "revision": "f663214ea08ca5a912a53f0fd5e68145",
     "url": "/static/media/NNLLs.f663214e.ipynb"
   },
@@ -44,40 +48,40 @@ self.__precacheManifest = [
     "url": "/static/media/Neurips2020.e836e643.ipynb"
   },
   {
-    "revision": "b96a8bab6ab118801d92ea6b6dc19baf",
-    "url": "/static/media/WhatIsATask.b96a8bab.ipynb"
+    "revision": "e1ae39ca6bb63818809f77e31f1535b3",
+    "url": "/static/media/WhatIsATask.e1ae39ca.ipynb"
   },
   {
     "revision": "2d2bb8ebc080c153f7c9099833685cd4",
     "url": "/static/media/crf_sampling.2d2bb8eb.ipynb"
   },
   {
-    "revision": "ff85e58810331c3ccb1ac93a4ebc3c67",
-    "url": "/static/media/phdiary.ff85e588.ipynb"
-  },
-  {
-    "revision": "ea3755c311d0678ca4f041912a9039d6",
-    "url": "/static/media/academicons.ea3755c3.eot"
-  },
-  {
-    "revision": "2552b354cabf01815628a244fde0636e",
-    "url": "/static/media/academicons.2552b354.woff"
-  },
-  {
-    "revision": "1afcbd6a1c454e7b7f0bc58155558b9d",
-    "url": "/static/media/academicons.1afcbd6a.ttf"
+    "revision": "644e80874685ff4b5e05890f658262be",
+    "url": "/static/media/phdiary.644e8087.ipynb"
   },
   {
     "revision": "406b38d29cffe8ffcb7962faa2e095ef",
     "url": "/static/media/academicons.406b38d2.svg"
   },
   {
-    "revision": "674f50d287a8c48dc19ba404d20fe713",
-    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+    "revision": "2552b354cabf01815628a244fde0636e",
+    "url": "/static/media/academicons.2552b354.woff"
+  },
+  {
+    "revision": "ea3755c311d0678ca4f041912a9039d6",
+    "url": "/static/media/academicons.ea3755c3.eot"
+  },
+  {
+    "revision": "1afcbd6a1c454e7b7f0bc58155558b9d",
+    "url": "/static/media/academicons.1afcbd6a.ttf"
   },
   {
     "revision": "af7ae505a9eed503f8b8e6982036873e",
     "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
   },
   {
     "revision": "fee66e712a8a08eef5805a46892932ad",
@@ -92,7 +96,7 @@ self.__precacheManifest = [
     "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
   },
   {
-    "revision": "43291ca788b88e10fbe6a9e86cb2543a",
+    "revision": "37c23d7abf5e9f3b07cccca5f5839b1f",
     "url": "/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.8f56c76230d3f67665022842377aa9c1.js"
+  "/precache-manifest.005d00d1a8c692bc3bcf499ad03e05e2.js"
 );
 
 workbox.clientsClaim();
